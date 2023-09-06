6bc386fab0f43793b7d8a5485acc2b7009967f93
IagoTrentiniEtur <140664391+IagoTrentiniEtur@users.noreply.github.com>
2023-09-06 20:50:53 -0300

    Update matriz.php

--- matriz.php
+++ matriz.php
@@ -1,3 +1,25 @@
 <php
+  $linhas = 10;
+  $colunas = 14;
+  for ($l=0; $l <= $linhas; $l++) {
+    for ($c=0; $c <= $colunas; $c++) {
+      $matriz[$l][$c] = rand(1, 1000);
+    }
+  }
+
+
   
+
+  
+  echo '<table>';
+  
+  for ($l=0; $l <= $linhas; $l++) {
+    echo '<tr>'
+    for ($c=0; $c <= $colunas; $c++) {
+      echo '<td>{$matriz[$l][$c]}</td>'
+    }
+    echo '</tr>'
+  }
+  
+  echo '</table>';
 ?>
