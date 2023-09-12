41c0b1ba6d80bad4cdeab855055c1b3e01e3b1d0
IagoTrentiniEtur <140664391+IagoTrentiniEtur@users.noreply.github.com>
2023-09-12 18:53:12 -0300

    Create ex1p.php

---
+++ ex1p.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Exercicio 1</title>
+	</head>
+	<body>
+		<?php
+                        
+                ?>
+	</body>
+</html>
