fdf4e38a5c4945dda11e2c7e515689b2e21cd72a
IagoTrentiniEtur <140664391+IagoTrentiniEtur@users.noreply.github.com>
2023-09-13 19:12:53 -0300

    Update ex6p.php

--- ex6p.php
+++ ex6p.php
@@ -7,7 +7,29 @@
 	</head>
 	<body>
 		<?php
-                        
+                        $poupanca = 50.00;
+			$precomaca = rand(1,10);
+			$quilomaca = 0.13;
+			$precomelancia = rand(1,10);
+			$quilomelancia = 10;
+			$precolaranja = rand(1,10);
+			$quilolaranja = 0.14;
+			$precorepolho = rand(1,10);
+			$quilorepolho = 0.63;
+			$precocenoura = rand(1,10);
+			$quilocenoura = 0.13;
+			$precobatata = rand(1,10);
+			$quilobatata = 0.15;
+			$precototalmaca = $precomaca * $quilomaca;
+			$precototalmelancia = $precomelancia * $quilomelancia;
+			$precototallaranja = $precolaranja * $quilolaranja;
+			$precototalrepolho = $precorepolho * $quilorepolho;
+			$precototalcenoura = $precocenoura * $quilocenoura;
+			$precototalbatata = $precobatata * $quilobatata;
+			$conta = $precototalmaca + $precototalmelancia + $precototallaranja + $precototalrepolho + $precototalcenoura + $precototalbatata;
+			if ($conta > $poupanca) {
+				echo 
+			}
                 ?>
 	</body>
 </html>
