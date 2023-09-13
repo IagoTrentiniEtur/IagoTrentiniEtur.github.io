96174003deb68e329c508a1b6ae3af32a3508738
IagoTrentiniEtur <140664391+IagoTrentiniEtur@users.noreply.github.com>
2023-09-13 19:20:22 -0300

    Create ex3p.php

---
+++ ex3p.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Exercicio 3</title>
+	</head>
+	<body>
+		<?php
+                        $lado = rand(1,10);
+                        
+                ?>
+	</body>
+</html>
