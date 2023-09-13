e65d2cc680abce18d0919da5b93cc009649208c7
IagoTrentiniEtur <140664391+IagoTrentiniEtur@users.noreply.github.com>
2023-09-13 18:38:41 -0300

    Update ex4p.php

--- ex4p.php
+++ ex4p.php
@@ -7,7 +7,14 @@
 	</head>
 	<body>
 		<?php
-                        
+                        $ladoa = rand(1,10);
+			$ladob = rand(1,10);
+			$area = $ladoa * $ladob;
+			if ($area > 10) {
+				echo "<h1>A área do retângulo de lados ".$ladoa." e ".$ladob." metros é ".$area." metros quadrados.</h1>";
+			} elseif ($area < 10) {
+				echo "<h3>A área do retângulo de lados ".$ladoa." e ".$ladob." metros é ".$area." metros quadrados.</h3>";
+			}
                 ?>
 	</body>
 </html>
