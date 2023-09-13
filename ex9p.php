2caecbb7172e0fbb89a5bd468a9405ce6760f92d
IagoTrentiniEtur <140664391+IagoTrentiniEtur@users.noreply.github.com>
2023-09-13 20:04:27 -0300

    Update ex9p.php

--- ex9p.php
+++ ex9p.php
@@ -7,7 +7,22 @@
 	</head>
 	<body>
 		<?php
-                        
+                        $capitalinicial = 4000.00;
+			$taxajuros = 4/100;
+			$tempo = 5;
+			$montantecomposto = $capitalinicial * ((1 + $taxajuros) ** $tempo);
+			echo "Capital Inicial: ".$capitalinicial."<br>";
+			echo "Taxa de Juros: ".$taxajuros."<br>";
+			echo "Tempo: ".$tempo."<br>";
+			echo "Montante Composto: ".$montantecomposto."<br>";
+			$jurocomposto = $montantecomposto - $capitalinicial;
+			echo "Juro Composto: ".$jurocomposto."<br>";
+			$jurosimples = $capitalinicial * $taxajuros * $tempo;
+			echo "Juro Simples: ".$jurosimples."<br>";
+			$montantesimples = $capitalinicial + $jurosimples;
+			echo "Montante Simples: ".$montantesimples."<br>";
+			$diferenca = $montantecomposto - $montantesimples;
+			echo "Diferença entre o composto e o simples: ".$diferenca;
                 ?>
 	</body>
 </html>
