975cc02bbab13344ce1cad331a29033984628119
IagoTrentiniEtur <140664391+IagoTrentiniEtur@users.noreply.github.com>
2023-10-25 19:37:09 -0300

    Update login.php

--- login.php
+++ login.php
@@ -29,6 +29,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Login</title>
 </head>
 <body>
